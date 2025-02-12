e2e94e6ee5dfdba567704fc494a86d6dca53b8ea
nanayawdev <nanayawisrael@gmail.com>
2025-02-12 23:00:33 +0000

    Update confirmation email template wording

--- components/email/confirmation-template.tsx
+++ components/email/confirmation-template.tsx
@@ -11,7 +11,7 @@ export function ConfirmationTemplate({ name, email, service }: ConfirmationTempl
       <div>
         <h1>Thank you for contacting NY Cre8ive Studio!</h1>
         <p>Hi ${name},</p>
-        <p>This email confirms that we&apos;ve received your inquiry about ${service}. I&apos;ll personally review it and get back to you at ${email} as soon as possible.</p>
+        <p>This email confirms that we&apos;ve received your inquiry about ${service}. I will personally review it and get back to you at ${email} as soon as possible.</p>
         <br />
         <p>Best regards,</p>
         <p>Israel Nana Yaw<br />NY Cre8ive Studio</p>
